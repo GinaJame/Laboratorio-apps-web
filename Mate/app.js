c83d17a60b432d8fa725391d3ccfa5dc00f63821
Georgina <gina10111@hotmail.com>
2022-02-27 21:04:04 -0600

    finish menu using a recursive function

--- Mate/app.js
+++ Mate/app.js
@@ -1,34 +1,42 @@
 var mateBasica = require('./mate')
 const readline = require("readline");
 
-/*console.log('El producto de x por a es igual a '+ mateBasica.producto(4,10));
-console.log(mateBasica.suma(3,4))
-console.log(mateBasica.resta(15,5))
-console.log(mateBasica.division(10,2))
-console.log(mateBasica.modulo(10,3))*/
+//CREAR INTERFAZ PARA INPUT Y OUTPUT
+const inquirer = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-//MENU 
-console.log('Bienvenido a la calculadora básica de Gina, ¿que quieres hacer ahora? (Ingresa el número)')
-var operacion = 9
-while(operacion !== 0){
-    operacion = prompt('1 Sumar \n 2 Restar \n 3 Multiplicar \n 4 Dividir \n 5 Modulo \n 0 Salir') 
-    var a = prompt('Ingresa tu a (primer numero) ')
-    var b = prompt('Ingresa tu b (segundo numero)')
+//FUNCION RECURSIVA PARA DEPEGAR EL MENU DE CALCULADORA Y HACER OPERACION
+var calcularRecursivo =function () {
+    //PREGUNTAR POR LA OPERACION
+    inquirer.question(" ¿Qué quieres hacer ahora? + - * / %  (Ingresa el operador o cualquier otra tecla para salir):  ", function (operacion) {
+        //SALIR SI NO ES UN OPERADOR VALIDO
+        if(operacion !== "+" && operacion !== "-" && operacion !== "/" && operacion !== "*" && operacion !== "%"){
+            return inquirer.close()
+        }
+        inquirer.question("Ingresa el primer número:  ", a => {
+            inquirer.question("Ingresa el segundo número:  ", b => {
+                if(operacion == "+"){
+                    console.log( 'La suma de a y b es igual a '+ mateBasica.suma(a,b))
+                }else if(operacion === "-"){
+                    console.log( 'La resta dea y b es igual a '+ mateBasica.resta(a,b))
+                }else if(operacion ==="*"){
+                    console.log( 'El producto de a por b es igual a '+ mateBasica.producto(a,b))
+                }else if(operacion ==="/"){
+                    console.log( 'La division de a por b es igual a '+ mateBasica.division(a,b))
+                }else if(operacion ==="%"){
+                    console.log( 'El modulo de a y b es igual a '+ mateBasica.modulo(a,b))
+                }
+                calcularRecursivo();
+            });
+        });
+    });
     
-    if(operacion == 1){
-       console.log( 'La suma de a y b es igual a '+ mateBasica.suma(a,b))
-    }else if(operacion ===2){
-        console.log( 'La resta dea y b es igual a '+ mateBasica.resta(a,b))
-    }else if(operacion ===3){
-        console.log( 'El producto de a por b es igual a '+ mateBasica.producto(a,b))
-    }else if(operacion ===4){
-        console.log( 'La division de a por b es igual a '+ mateBasica.division(a,b))
-    }else if(operacion ===5){
-        console.log( 'El modulo de a y b es igual a '+ mateBasica.modulo(a,b))
-    }else if(operacion === 0){
-        console.log('ADIOS')
-    }else {
-        console.log('No es una opción disponible')
-    }
-}
+};
+
+
+console.log("Bienvenido a la calculadora básica de Gina")
+calcularRecursivo();
+
 
